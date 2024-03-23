94e43f6d337f2c95934cb98216578b67c277ddea
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-03-23 18:47:04 +0200

    hw-02

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
   
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  const totalPrice = quantity * pricePerDroid;
+
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
-console.log( makeTransaction(5, 3000) );
-console.log( makeTransaction(3, 1000) );
-console.log( makeTransaction(10, 500) );
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,15 @@
-function getElementWidth(content, padding, border) {
-  
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-
-  const totalWidth = contentWidth + (paddingWidth + borderWidth) * 2;
-  return totalWidth;
+function checkForSpam(message) {
+  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
-console.log( getElementWidth("50px", "8px", "4px") );
-console.log( getElementWidth("60px", "12px", "8.5px") );
-console.log( getElementWidth("200px", "0px", "0px") );
-console.log( getElementWidth("700px", "24px", "14.5px") );
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
